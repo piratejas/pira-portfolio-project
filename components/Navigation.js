d92ec52c60aae10b3ea42a0695de74573c296e84
piratejas <47509562+piratejas@users.noreply.github.com>
2022-08-18 01:01:14 +0100

    added custom colours; h1 & h2; navbar sections

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,19 +1,48 @@
 import Link from "next/link";
 import React from "react";
+import ThemeSwitch from "./ThemeSwitch";
 
 const Navigation = () => {
   return (
-    <div className="sticky top-0 z-20 py-2 bg-white md:py-6 md:mb-6 dark:bg-black">
-      <div className="container px-4 mx-auto lg:max-w-4xl flex items-center justify-between">
+    <div className="sticky top-0 z-20 py-2 bg-orange md:py-6 md:mb-6 dark:bg-orange">
+      <div className="container px-4 font-bold mx-auto lg:max-w-4xl flex items-center justify-between">
         <Link href="/">
           <a
             className={
-              "font-medium tracking-wider transition-colors text-gray-900 hover:text-sky-500 uppercase dark:text-white"
+              "font-medium tracking-wider transition-colors text-black hover:text-orange dark:text-white"
             }
           >
-            Your Name
+            about
           </a>
         </Link>
+        <Link href="/">
+          <a
+            className={
+              "font-medium tracking-wider transition-colors text-black hover:text-orange dark:text-white"
+            }
+          >
+            work
+          </a>
+        </Link>
+        <Link href="/">
+          <a
+            className={
+              "font-medium tracking-wider transition-colors text-black hover:text-orange dark:text-white"
+            }
+          >
+            stack
+          </a>
+        </Link>
+        <Link href="/">
+          <a
+            className={
+              "font-medium tracking-wider transition-colors text-black hover:text-orange dark:text-white"
+            }
+          >
+            contact
+          </a>
+        </Link>
+        <ThemeSwitch />
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
+    extend: {},
+    colors: {
+      white: "#FBFEF9",
+      black: "#191923",
+      orange: "#FF7F11",
+      grey: "#998DA0",
+      green: "#307473",
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
+};
