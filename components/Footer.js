912ad178ab7bcc7178f3e1e1410d6d0edc788cb8
piratejas <47509562+piratejas@users.noreply.github.com>
2022-08-19 11:47:21 +0100

    changed footer padding

--- components/Footer.js
+++ components/Footer.js
@@ -4,10 +4,10 @@ const Footer = () => {
   return (
     <div className="mt-12 lg:mt-18 sm:pb-36 sm:py-12 py-6">
       <div className="max-w-4xl px-4 mx-auto text-black dark:text-white">
-        <div className="pb-8 mb-2 border-t-2 border-black dark:border-white"></div>
-        <div className="flex flex-col justify-between lg:flex-row items-center">
+        <div className="pb-4 mb-2 border-t-2 border-black dark:border-white"></div>
+        <div className="flex flex-col-reverse justify-between lg:flex-row items-center">
           <p>Built with Next.js, Tailwind and Vercel</p>
-          <div className="flex flex-wrap pt-2 sm:space-x-4 space-x-2 font-bold lg:pt-0">
+          <div className="flex flex-wrap pt-0 pb-2 sm:space-x-4 space-x-2 font-bold">
             <a
               href="https://www.codewars.com/users/piratejas"
               className={"transition-colors hover:text-orange"}
