ad725b3fdf89b177395db30d427e70b3ae27f5c1
piratejas <47509562+piratejas@users.noreply.github.com>
2022-09-03 11:08:11 +0700

    removed drop shadow from avatar img; added logos for footer links; add conditional rendering of links/text in footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,38 +1,78 @@
 import React from "react";
+import { useTheme } from "next-themes";
 
 const Footer = () => {
+  const { theme, setTheme, resolvedTheme } = useTheme();
   return (
     <div className="sm:py-12 py-6">
       <div className="max-w-4xl px-4 mx-auto text-black dark:text-white">
-        <div className="pb-4 mb-2 border-t-2 border-black dark:border-white"></div>
+        <div className="pb-0 mb-2 border-t-2 border-black dark:border-white"></div>
         <div className="flex flex-col-reverse justify-between lg:flex-row items-center">
           <p>Built with Next.js, Tailwind and Vercel</p>
-          <div className="flex flex-wrap pt-0 pb-2 sm:space-x-4 space-x-2 font-bold">
-            <a
-              href="https://www.codewars.com/users/piratejas"
-              className={"transition-colors hover:text-orange"}
-              target="_blank"
-              rel="noreferrer"
-            >
-              CodeWars
-            </a>
-            <a
-              href="https://github.com/piratejas"
-              className={"transition-colors hover:text-orange"}
-              target="_blank"
-              rel="noreferrer"
-            >
-              GitHub
-            </a>
-            <a
-              href="https://www.linkedin.com/in/piratejas/"
-              className={"transition-colors hover:text-orange"}
-              target="_blank"
-              rel="noreferrer"
-            >
-              LinkedIn
-            </a>
-          </div>
+          {theme === "dark" || resolvedTheme === "dark" ? (
+            <div className="flex flex-wrap pt-0 pb-2 sm:space-x-4 space-x-2 font-bold">
+              <a
+                href="https://www.codewars.com/users/piratejas"
+                target="_blank"
+                rel="noreferrer"
+                className="transition-transform hover:text-orange"
+              >
+                codeWars
+              </a>
+              <a
+                href="https://github.com/piratejas"
+                target="_blank"
+                rel="noreferrer"
+                className="transition-transform hover:text-orange"
+              >
+                gitHub
+              </a>
+              <a
+                href="https://www.linkedin.com/in/piratejas/"
+                target="_blank"
+                rel="noreferrer"
+                className="transition-transform hover:text-orange"
+              >
+                linkedIn
+              </a>
+            </div>
+          ) : (
+            <div className="flex flex-wrap pt-0 pb-2 sm:space-x-4 space-x-2 font-bold">
+              <a
+                href="https://www.codewars.com/users/piratejas"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="/codewars.png"
+                  alt="codeWars logo"
+                  className="rounded-full h-10 w-10 p-px object-scale-down transition-transform ease-in-out hover:scale-110 bg-white"
+                />
+              </a>
+              <a
+                href="https://github.com/piratejas"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="/gitHub.png"
+                  alt="gitHub logo"
+                  className="rounded-full h-10 w-10 p-1 object-scale-down transition-transform ease-in-out hover:scale-110 bg-white"
+                />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/piratejas/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="/linkedIn.png"
+                  alt="linkedIn logo"
+                  className="rounded-full h-10 w-10 p-px object-scale-down transition-transform ease-in-out hover:scale-110 bg-white"
+                />
+              </a>
+            </div>
+          )}
         </div>
       </div>
     </div>
